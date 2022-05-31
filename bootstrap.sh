bd02fae27eabd86123adfdb12d71af741889fad7
Alexandre Schwartzmann <schwartzmann.a@gmail.com>
2022-05-31 17:51:26 +0200

    update awscli

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,14 +3,23 @@
 # retrieve dependancies
 curl https://raw.githubusercontent.com/nex84/arcanexus-bootstrap/master/packagelist_yum -o /tmp/packagelist_yum
 curl https://raw.githubusercontent.com/nex84/arcanexus-bootstrap/master/packagelist_pip3 -o /tmp/packagelist_pip3
+curl https://raw.githubusercontent.com/nex84/arcanexus-bootstrap/master/ansible_collections -o /tmp/ansible_collections
 #activate repos
 amazon-linux-extras install -y epel
 
 yum makecache -y 
 yum update -y *
-yum install -y $(cat /tmp/packagelist_yum)
+yum install -y $(cat /tmp/packagelist_yum | egrep -v 'ˆ#')
 
-pip3 install -U $(cat /tmp/packagelist_pip3)
+pip3 install -U $(cat /tmp/packagelist_pip3 | egrep -v 'ˆ#')
+
+ansible-galaxy collection install $(cat /tmp/ansible_collections | egrep -v 'ˆ#')
+
+# update awscli to v2
+yum remove awscli -y
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install
 
 export AWS_DEFAULT_REGION=`curl -s http://169.254.169.254/latest/dynamic/instance-identity/document|jq -r .region`
 echo "AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION}" | tee -a /etc/environment
@@ -42,9 +51,10 @@ do
 done
 
 # Install Datadog Agent
-/usr/local/bin/ansible-galaxy role install  Datadog.datadog
+DATADOG_API_KEY=`aws ssm get-parameter --name "gandi_api_key" --with-decryption | jq -r .Parameter.Value`
+/usr/local/bin/ansible-galaxy install Datadog.datadog
 # /usr/local/bin/ansible-playbook /opt/scripts/ansible/datadog/install.yml
-/usr/local/bin/ansible-playbook /opt/scripts/ansible/datadog/install_manual.yml
+/usr/local/bin/ansible-playbook /opt/scripts/ansible/datadog/install_manual.yml -e datadog_api_key="${DATADOG_API_KEY}"
 
 # CloudWatch Agent
 /usr/local/bin/ansible-playbook /opt/scripts/ansible/amazon-cloudwatch-agent/install.yml
