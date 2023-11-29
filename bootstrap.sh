cf3dfd92812ce5b1d735a3a07f66a888dbabe5a4
-=NeXuS=- <schwartzmann.a@gmail.com>
2023-11-29 11:23:46 +0100

    more debug

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,7 @@ OS=
 PKG_MANAGER=
 
 # retrieve dependancies
+echo "====== [ BASE : Retrieve dependancies ] ======"
 # packages
 curl -s https://raw.githubusercontent.com/nex84/arcanexus-bootstrap/master/packagelist_yum -o /tmp/packagelist_yum
 curl -s https://raw.githubusercontent.com/nex84/arcanexus-bootstrap/master/packagelist_apt -o /tmp/packagelist_apt
@@ -15,6 +16,7 @@ curl -s https://raw.githubusercontent.com/nex84/arcanexus-bootstrap/master/ansib
 curl -s https://raw.githubusercontent.com/nex84/arcanexus-bootstrap/master/bootstrap_aws.sh -o /tmp/bootstrap_aws.sh ; chmod +x /tmp/bootstrap_aws.sh
 
 # detect running platform
+echo "====== [ BASE : Detect running platform ] ======"
 if curl -s -m 2 http://169.254.169.254/latest/meta-data/ >/dev/null 2>&1; then
   PLATFORM="aws"
 elif curl -s -m 2 http://169.254.169.254/metadata/instance/compute?api-version=2021-02-01 >/dev/null 2>&1; then
@@ -28,6 +30,7 @@ echo "PLATFORM=\"$PLATFORM\"" | sudo tee -a /etc/environment
 export PLATFORM
 
 # detect packages manager and install packages 
+echo "====== [ BASE : Detect package manager ] ======"
 OS=`grep -e '^ID=' /etc/os-release | cut -d= -f2`
 echo "OS=\"$OS\"" | sudo tee -a /etc/environment
 export OS
@@ -43,7 +46,9 @@ case $OS in
       export PKG_MANAGER="dnf"
       echo 'PKG_MANAGER="dnf"' | sudo tee -a /etc/environment
     fi
+    
     #activate amazon repos
+    echo "====== [ BASE : Install base packages ] ======"
     sudo amazon-linux-extras install -y epel python3.8
     sudo ${PKG_MANAGER} makecache -y 
     sudo ${PKG_MANAGER} update -y
@@ -55,6 +60,8 @@ case $OS in
   debian|ubuntu)
     export PKG_MANAGER="apt"
     echo 'PKG_MANAGER="apt"' | sudo tee -a /etc/environment
+    
+    echo "====== [ BASE : Install base packages ] ======"
     curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
     echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
     sudo apt update
@@ -68,9 +75,11 @@ case $OS in
 esac
 
 # install Ansible collections
+# echo "====== [ BASE : Install Ansible collections ] ======"
 # sudo ansible-galaxy collection install $(cat /tmp/ansible_collections | egrep -v '^#')
 
 # update awscli to v2
+echo "====== [ BASE : Update AWS CLI ] ======"
 AWSCLI_VERSION=`aws --version 2> /dev/null | cut -d ' ' -f1 | cut -d '/' -f2 | cut -d '.' -f1`
 curl "https://awscli.amazonaws.com/awscli-exe-linux-`uname -m`.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
@@ -81,38 +90,48 @@ else
 fi
 
 # Workaround : oeuf/poule
+echo "====== [ BASE : Workaround : get scripts ] ======"
 GIT_PAT_TOKEN=`aws ssm get-parameter --name "git_pat_token" --with-decryption | jq -r .Parameter.Value`
 sudo mkdir -p /opt/
 cd /opt
 sudo git clone https://nex84:${GIT_PAT_TOKEN}@github.com/nex84/scripts.git
 
 # nexus user
+echo "====== [ BASE : Create user : nexus ] ======"
 sudo ansible-playbook /opt/scripts/ansible/Common/init_linux_user.yaml -e user_name=nexus -e user_password=`aws ssm get-parameter --name "default_password" --with-decryption | jq -r .Parameter.Value`  -e user_sudogroup=sudo -e user_nopasswd=false
 # rundeck user
+echo "====== [ BASE : Create user : rundeck ] ======"
 sudo ansible-playbook /opt/scripts/ansible/Common/init_linux_user.yaml -e user_name=rundeck -e user_password=`aws ssm get-parameter --name "default_password" --with-decryption | jq -r .Parameter.Value`  -e user_sudogroup=sudo -e user_nopasswd=true
 
 #retrieve scripts
+echo "====== [ BASE : Deploy scripts ] ======"
 GIT_PAT_TOKEN=`aws ssm get-parameter --name "git_pat_token" --with-decryption | jq -r .Parameter.Value`
 curl -X POST \
   -H "Authorization: token ${GIT_PAT_TOKEN}" \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/nex84/scripts/actions/workflows/deployToEC2.yml/dispatches \
   -d '{"ref":"master"}'
+sleep 2m
 
 # launch platform specific steps
+echo "====== [ BASE : Launch ${PLATFORM} specific script ] ======"
 /tmp/bootstrap_${PLATFORM}.sh
 
 # Install Datadog Agent
+echo "====== [ BASE : Install Datadog Agent ] ======"
 DATADOG_API_KEY=`aws ssm get-parameter --name "datadog_api_key" --with-decryption | jq -r .Parameter.Value`
 ansible-galaxy install Datadog.datadog
 # ansible-playbook /opt/scripts/ansible/datadog/install.yml
 ansible-playbook /opt/scripts/ansible/datadog/install_manual.yml -e datadog_api_key="${DATADOG_API_KEY}"
 
 # Install Prometheus Node-exporter
+echo "====== [ BASE : Install Prometheus node-exporter ] ======"
 ansible-playbook /opt/scripts/ansible/prometheus-node-exporter/install.yml
 
 #launch cloud init scripts
+echo "====== [ BASE : Launch Common script ] ======"
 /opt/scripts/$(echo "$PLATFORM" | tr '[:lower:]' '[:upper:]')/cloud-init/common.sh
 
 # Final Report
+echo "====== [ BASE : Send report ] ======"
 ansible-playbook /opt/scripts/ansible/Common/cloud-init-report.yml
--- bootstrap_aws.sh
+++ bootstrap_aws.sh
@@ -4,6 +4,7 @@
 AWS_DEFAULT_REGION=
 
 # determine default AWS region
+echo "====== [ AWS : Determine default region ] ======"
 AWS_CURRENT_REGION=`curl -s http://169.254.169.254/latest/dynamic/instance-identity/document|jq -r .region`
 if [ "$AWS_CURRENT_REGION" != "" ]
 then
@@ -14,10 +15,12 @@ fi
 echo "AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION}" | sudo tee -a /etc/environment
 
 # CodeDeploy Agent
+echo "====== [ AWS : Install CodeDeploy Agent ] ======"
 curl https://aws-codedeploy-eu-west-1.s3.amazonaws.com/latest/install -o /tmp/install
 chmod +x /tmp/install 
 sudo /tmp/install auto 
 sudo service codedeploy-agent start 
 
 # CloudWatch Agent
+echo "====== [ AWS : Install Cloudwatch Agent ] ======"
 ansible-playbook /opt/scripts/ansible/amazon-cloudwatch-agent/install.yml
