380af834e6a783b0130bffb7528870b2140d914c
-=NeXuS=- <schwartzmann.a@gmail.com>
2024-04-10 07:53:52 +0200

    rights

--- bootstrap.sh
+++ bootstrap.sh
@@ -64,7 +64,9 @@ case $OS in
     
     echo "====== [ BASE : Install base packages ] ======"
     curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+    sudo chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg # allow unprivileged APT programs to read this keyring
     echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+    sudo chmod 644 /etc/apt/sources.list.d/kubernetes.list   # helps tools such as command-not-found to work correctly
     sudo add-apt-repository --yes --update ppa:ansible/ansible
     sudo apt update
     sudo apt install -y $(cat /tmp/packagelist_apt | egrep -v '^#')
