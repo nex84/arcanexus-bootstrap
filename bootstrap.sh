7ee737f5bb9982f0c1189552b54933f5a493fa77
-=NeXuS=- <schwartzmann.a@gmail.com>
2024-04-11 10:53:47 +0200

    add helm

--- bootstrap.sh
+++ bootstrap.sh
@@ -63,11 +63,17 @@ case $OS in
     echo 'PKG_MANAGER="apt"' | sudo tee -a /etc/environment
     
     echo "====== [ BASE : Install base packages ] ======"
+    # add kubernetes
     curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
     sudo chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg # allow unprivileged APT programs to read this keyring
     echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
     sudo chmod 644 /etc/apt/sources.list.d/kubernetes.list   # helps tools such as command-not-found to work correctly
+    # add ansible
     sudo add-apt-repository --yes --update ppa:ansible/ansible
+    # add helm
+    curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+
     sudo apt update
     sudo apt install -y $(cat /tmp/packagelist_apt | egrep -v '^#')
     sudo apt dist-upgrade -y
