8656c75e62a965b605f2de86711f1f5eb5c9ee97
nex84 <schwartzmann.a@gmail.com>
2024-08-30 09:04:49 +0200

    add logs

--- bootstrap.sh
+++ bootstrap.sh
@@ -68,27 +68,36 @@ case $OS in
     
     echo "====== [ BASE : Install base packages ] ======"
     # add kubernetes
+    echo "Add Kubernetes repository"
     curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --batch --yes --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
     sudo chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg # allow unprivileged APT programs to read this keyring
     echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
     sudo chmod 644 /etc/apt/sources.list.d/kubernetes.list   # helps tools such as command-not-found to work correctly
     # add ansible
+    echo "Add Ansible repository"
     sudo add-apt-repository --yes --update ppa:ansible/ansible
     # add helm
+    echo "Add Helm repository"
     curl https://baltocdn.com/helm/signing.asc | gpg --batch --yes --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
     # add terraform
+    echo "Add Hashicorp repository"
     wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --batch --yes --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
     echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 
+    echo "Update and install packages"
     sudo apt update
     sudo apt install -y $(cat /tmp/packagelist_apt | egrep -v '^#')
     sudo apt dist-upgrade -y
     sudo apt autoremove -y
     sudo apt autoclean -y
+
+    echo "Update and install Python packages"
     sudo pip3 install -U $(cat /tmp/packagelist_pip3 | egrep -v '^#')
     ;;
 esac
+
+echo "Update and install Ansible Collections"	
 ansible-galaxy collection install $(cat /tmp/ansible_collections | egrep -v '^#')
 
 
